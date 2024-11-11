20d8b844c6167a00e3795937589fde5940d0ce22
Ribal Baghdadi <115172599+RibalBaghdadi@users.noreply.github.com>
2024-11-11 13:03:51 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ribalbaghdadi:1990ribal@cluster0.umrx9.mongodb.net/food-del').then(() => console.log("DB Connected"));
+    await mongoose.connect(mongodb_url);
 
 }
