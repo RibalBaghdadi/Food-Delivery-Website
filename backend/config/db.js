9f6ebe9a5e2792b4276b995078ca99f5f6024f8a
Ribal Baghdadi <115172599+RibalBaghdadi@users.noreply.github.com>
2024-12-22 10:00:33 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect(mongodb_url);
+    await mongoose.connect("mongodb+srv://ribalbaghdadi:ribalbaghdadi@cluster0.umrx9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 }
